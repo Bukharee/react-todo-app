5f093a3a98421e0e330e56dcbe2d913dbc0d19c8
bukharee <idrisbukhariauwal@gmail.com>
2021-07-23 17:49:25 +0100

    implement delete todo

--- src/App.js
+++ src/App.js
@@ -2,28 +2,53 @@ import './App.css';
 import Todo from "./Todo";
 import { useState } from "react";
 
-const TODOS = [{text:'finish eama', id:1}, 
-{text:'go back to school', id:2}, 
-{text:'carry out some things to market', id:3}]
+const TODOS = []
 
 
 function App() {
-  const [todo, setTodo] = useState("kano")
-  const addTodo = () => {
-    
+  const [todos, setTodos] = useState([{text:'finish eama', id:1, done:false}, 
+  {text:'go back to school', id:2, done:false}, 
+  {text:'carry out some things to market', id:3, done:false}])
+  const [todo, setTodo] = useState("im trying my best to learn react");
+  function onSubmit(e) {
+    e.preventDefault()
+    console.log("nayi submitting")
+    if (!todo) {
+      alert('add a task');
+    } else {
+      console.log('one ')
+      setTodos(
+        prev => {
+          return [...prev, {text: todo, id:4}]
+        }
+      );
+      setTodo("")
+    }
   }
+  function deleteTodo(id) {
+    setTodos(
+      todos.filter(
+        (my_todo) => 
+          my_todo.id !== id
+           )
+        )
+    
+    console.log(`delete${id}`)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
       {
-        TODOS.map((todo) => (
-          <Todo text={todo.text} key={todo.id} />
+        todos.map((todo) => (
+          <Todo text={todo.text} key={todo.id} onDelete={deleteTodo} todo={todo.id}/>
         ))
       }
       </header>
-      <form>
+      <form onSubmit={onSubmit}>
       <input value={todo} type="text" onChange={ (e) => { setTodo(e.target.value)}}/>
-      <button id="submit">Submit</button>
+      <button id="submit" onClick={onSubmit}>Submit</button>
       </form>
     </div>
   );
--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,11 @@
 // the todo component 
-const Todos = (props) => {
-    
-}
 
 
 const Todo = (props) => {
   return (
     <div className='Todo'>
-    <p className="todo-text">{props.text} <input type="checkbox" /></p>
+    <p className="todo-text">{props.text} <input type="checkbox" style={{backgroundColor: 'orange', 
+    color: 'red'}}/> <button className="delete" onClick={() => props.onDelete(props.todo)}>delete</button></p>
     </div>
   )
 }
